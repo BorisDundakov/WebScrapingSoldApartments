8b9e715685788181b6b5b1244c01704b580528b4
Boris Dundakov <71731579+BorisDundakov@users.noreply.github.com>
2022-07-08 16:15:17 +0300

    Delete Apartment.py

--- Apartment.py
+++
@@ -1,8 +0,0 @@
-class Apartment:
-    def __init__(self, id, city, address, square_m, n_bedrooms):
-        self.__id = id
-        self.city = city
-        self.address = address
-        self.square_m = square_m
-        self.n_bedrooms = n_bedrooms
-
